c9100e61c4f8f43efd46d48b8827d77281434ddb
Iryna Lazareva <il337733@gmail.com>
2022-09-15 20:45:18 +0300

    change

--- src/fetchPhoto.js
+++ src/fetchPhoto.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '29839947-203cd2765f14246beba1c6e54';
+// const BASE_URL = 'https://pixabay.com/api/';
+// const KEY = '29839947-203cd2765f14246beba1c6e54';
 
-export const fetchPhoto = (inputValue) => {
-const url = `${BASE_URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`;
-return axios.get(url)
-}
+// function fetchPhoto (searchQuery) {
+// const url = `${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+// return axios.get(url)
+//     // return fetch(url).then(r => r.json()).then(console.log)
+// }
+// export default { fetchPhoto }
+
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import axios from "axios";
-import { fetchPhoto } from "./fetchPhoto";
+import PhotoApiService from "./fetchPhoto";
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
   photosContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
+let page = 1
+let limit = 40
+let searchQuery = ''
+//запрос на сервер
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '29839947-203cd2765f14246beba1c6e54';
+
+async function fetchPhoto(searchQuery, page) {
+  const url = `${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+  const photoList = await axios.get(url).then(response => response.data);
+  return photoList
+}
+
+
 //слушатель событий на форме
 refs.searchForm.addEventListener('submit', onSearch)
+refs.loadMoreBtn.addEventListener('click', onLoadMore)
+
 
 function onSearch(evt) {
-    evt.preventDefault()
-    const form = evt.currentTarget.elements.searchQuery.value
-    console.log(form)
+  evt.preventDefault();
+  //значение инпута, идет в запрос на сервер
+  searchQuery = evt.currentTarget.elements.searchQuery.value;
+  refs.loadMoreBtn.classList.add('is-hidden');
+ console.log(searchQuery)
+
+  if (searchQuery === '') {
+    return
+  }
+  
+}
+
+function onLoadMore() {
+  
+
 }
