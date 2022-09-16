9707786c70deca6f9fc0b68a4b6a57dff3b16df2
Iryna Lazareva <il337733@gmail.com>
2022-09-16 21:08:13 +0300

    add

--- src/fetchPhoto.js
+++ src/fetchPhoto.js
@@ -8,6 +8,14 @@ import axios from "axios";
 // return axios.get(url)
 //     // return fetch(url).then(r => r.json()).then(console.log)
 // }
-// export default { fetchPhoto }
+// 
 
 
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '29839947-203cd2765f14246beba1c6e54';
+
+export const fetchPhoto = (searchQuery, page, perPage) => {
+  const url = `${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+  return axios.get(url);
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,44 +1,114 @@
-import axios from "axios";
-import PhotoApiService from "./fetchPhoto";
+import axios from 'axios';
+import { fetchPhoto } from './fetchPhoto';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
   photosContainer: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
-let page = 1
-let limit = 40
-let searchQuery = ''
-//запрос на сервер
-const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '29839947-203cd2765f14246beba1c6e54';
-
-async function fetchPhoto(searchQuery, page) {
-  const url = `${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
-  const photoList = await axios.get(url).then(response => response.data);
-  return photoList
-}
-
+let page = 1;
+const PER_PAGE = 40;
+let searchQuery = '';
+let lightbox = new SimpleLightbox('.photo-card a');
 
 //слушатель событий на форме
-refs.searchForm.addEventListener('submit', onSearch)
-refs.loadMoreBtn.addEventListener('click', onLoadMore)
 
-
-function onSearch(evt) {
+const onSearch = async evt => {
   evt.preventDefault();
   //значение инпута, идет в запрос на сервер
-  searchQuery = evt.currentTarget.elements.searchQuery.value;
   refs.loadMoreBtn.classList.add('is-hidden');
- console.log(searchQuery)
+  searchQuery = evt.currentTarget.elements.searchQuery.value;
+  console.log(searchQuery);
+  refs.photosContainer.innerHTML = '';
 
   if (searchQuery === '') {
-    return
+    return;
   }
+
+  try {
+    const { data } = await fetchPhoto(searchQuery, page, PER_PAGE);
+    console.log(data.totalHits);
+    page = 1;
+
+    if (data.totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    renderPhoto(data.hits);
+    
+    lightbox.refresh();
+    refs.loadMoreBtn.classList.remove('is-hidden');
+  } catch (error) {
+    console.log(error);
+  }
+
   
+  
+};
+
+
+function renderPhoto(photos) {
+  const markup = photos
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+        <a class="gallery-link" href="${largeImageURL}">
+  <img src="${webformatURL}"  alt="${tags}" loading="lazy" class="gallery-photo"/></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</div>`;
+      }
+    )
+    .join('');
+  refs.photosContainer.insertAdjacentHTML('beforeend', markup);
 }
 
-function onLoadMore() {
-  
+const onLoadMore = async () => {
+  try {
+    const { data } = await fetchPhoto(searchQuery, page, PER_PAGE);
+    const countPage = data.totalHits / PER_PAGE;
+    console.log(countPage);
+    if (page >= countPage) {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      refs.loadMoreBtn.classList.add('is-hidden');
+      return;
+    }
+    renderPhoto(data.hits);
+    lightbox.refresh();
+    page += 1;
+    console.log(page);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-}
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
